696a7c53a61aae1f40c56091b74dd96fb934cf7a
Esun Kim <veblush@gmail.com>
2016-02-13 15:36:33 +0900

    Add log4net extensions

---
+++ extensions/Log4net.Logstash/LogstashAppender.cs
@@ -0,0 +1,67 @@
+﻿using System.ComponentModel;
+using System.Collections.Generic;
+using LumberjackClient;
+using System;
+using log4net.Appender;
+using log4net.Core;
+
+namespace Log4net.Logstash
+{
+    public sealed class LogstashAppender : AppenderSkeleton
+    {
+        private LumberjackClientSettings _settings;
+        private LumberjackClient.LumberjackClient _client;
+
+        public string Host { get; set; }
+        public int Port { get; set; }
+
+        [DefaultValue(10)]
+        public int ConnectRetryCount { get; set; } = 10;
+
+        [DefaultValue(65536)]
+        public int SendBufferSize { get; set; } = 65536;
+
+        [DefaultValue(4096)]
+        public int ReceiveBufferSize { get; set; } = 4096;
+
+        [DefaultValue(LumberjackClientSettings.SendFullPolicy.Drop)]
+        public LumberjackClientSettings.SendFullPolicy SendFull { get; set; } = LumberjackClientSettings.SendFullPolicy.Drop;
+
+        [DefaultValue(LumberjackClientSettings.SendConfirmPolicy.Receive)]
+        public LumberjackClientSettings.SendConfirmPolicy SendConfirm { get; set; } = LumberjackClientSettings.SendConfirmPolicy.Receive;
+
+        public override void ActivateOptions()
+        {
+            base.ActivateOptions();
+
+            _settings = new LumberjackClientSettings
+            {
+                Host = Host,
+                Port = Port,
+                ConnectRetryCount = ConnectRetryCount,
+                SendBufferSize = SendBufferSize,
+                ReceiveBufferSize = ReceiveBufferSize,
+                SendFull = SendFull,
+                SendConfirm = SendConfirm,
+            };
+
+            _client = new LumberjackClient.LumberjackClient(_settings);
+        }
+
+        protected override void OnClose()
+        {
+            // TODO: Dispose _client to flush all pending data
+
+            base.OnClose();
+        }
+
+        protected override void Append(LoggingEvent loggingEvent)
+        {
+            _client.Send(
+                new KeyValuePair<string, string>("logger", loggingEvent.LoggerName),
+                new KeyValuePair<string, string>("level", loggingEvent.Level.ToString()),
+                new KeyValuePair<string, string>("host", Environment.MachineName),
+                new KeyValuePair<string, string>("message", RenderLoggingEvent(loggingEvent)));
+        }
+    }
+}
---
+++ samples/Log4net/Program.cs
@@ -0,0 +1,44 @@
+﻿using log4net;
+using log4net.Layout;
+using log4net.Repository.Hierarchy;
+using Log4net.Logstash;
+using System;
+using System.Threading;
+
+namespace Log4net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Setup();
+            TestLog();
+            Thread.Sleep(1000);
+        }
+
+        static void Setup()
+        {
+            var hierarchy = (Hierarchy)LogManager.GetRepository();
+
+            var patternLayout = new PatternLayout();
+            patternLayout.ConversionPattern = "%m %exception";
+            patternLayout.ActivateOptions();
+
+            var logstashAppender = new LogstashAppender();
+            logstashAppender.Host = "localhost";
+            logstashAppender.Port = 5000;
+            logstashAppender.Layout = patternLayout;
+            logstashAppender.ActivateOptions();
+            log4net.Config.BasicConfigurator.Configure(logstashAppender);
+        }
+
+        static void TestLog()
+        {
+            var logger = LogManager.GetLogger("Test");
+            logger.Debug("Test Debug Log");
+            logger.Info("Test Info Log");
+            logger.Warn("Test Warn Log");
+            logger.Error("Test Error Log", new Exception("Exception Message"));
+        }
+    }
+}
